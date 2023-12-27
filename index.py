8b3ce775fba93e2da234abca41586be5dee9eb59
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-27 14:20:43 -0600

    i dont wanna walk around

--- index.py
+++ index.py
@@ -6,9 +6,17 @@ uploaded_file = st.file_uploader("Choose a CSV file")
 
 if uploaded_file:
 
-    st.write(uploaded_file.name)
+    fileName = uploaded_file.name
     
-    df = pd.read_csv(uploaded_file)
+    st.write(fileName)
+    
+    if fileName.endswith("csv"):
+        
+        df = pd.read_csv(uploaded_file)
+
+    elif fileName.endswith("xlsx"):
+
+        df = pd.read_excel(uploaded_file)
 
     st.write("### Uploaded CSV File:")
     
