4d719ca0700c13743334357a4e5fcb2ae1d1dc8a
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-27 14:33:53 -0600

    commas to dots

--- index.py
+++ index.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
 
+df = {
+    'Fecha': ['1/1/22 0:00', '1/1/22 1:00', '1/1/22 2:00'],
+    'MP2.5 (µg/m³)': [25.6, 7.0, 5.4],
+    'Velocidad del viento (m/s)': [3.0, 2.6, 1.8],
+    'Altura de Mezcla (m)': [124, 78, 52]
+}
+
 uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 
 if uploaded_file:
@@ -11,12 +18,16 @@ if uploaded_file:
 
     st.write(df)
 
-    # st.write(uploaded_file.name)
+    st.write(uploaded_file.name)
 
-    st.write(f'''
-    {df[df.columns[1]].sum()}
+df[df.columns[2]] = df[df.columns[2]].replace(",",".")
 
-    {df[df.columns[2]].sum()}
-    
-    {df[df.columns[3]].sum()}
-    ''')
+st.write(f'''
+
+{df[df.columns[1]].sum()}
+
+{df[df.columns[2]].sum()}
+
+{df[df.columns[3]].sum()}
+
+''')
