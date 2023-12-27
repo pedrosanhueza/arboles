809a2fdf11452372b0f260a11930c4edc17f3c4a
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-27 14:36:27 -0600

    only you

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-df = {
+sample_data = {
     'Fecha': ['1/1/22 0:00', '1/1/22 1:00', '1/1/22 2:00'],
     'MP2.5 (µg/m³)': [25.6, 7.0, 5.4],
     'Velocidad del viento (m/s)': [3.0, 2.6, 1.8],
     'Altura de Mezcla (m)': [124, 78, 52]
 }
+
+df = pd.DataFrame(sample_data)
 
 uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 
