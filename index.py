48ce67884b1b9250bf2a33b8c5a8c196daf1f2fa
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-27 14:45:57 -0600

    rap cada ve mas de rap

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import pandas as pd
 sample_data = {
     'Fecha': ['1/1/22 0:00', '1/1/22 1:00', '1/1/22 2:00'],
     'MP2.5 (µg/m³)': [25.6, 7.0, 5.4],
-    'Velocidad del viento (m/s)': [3.0, 2.6, 1.8],
+    'Velocidad del viento (m/s)': ["3,0", "2,6", "1,8"],
     'Altura de Mezcla (m)': [124, 78, 52]
 }
 
@@ -21,6 +21,8 @@ if uploaded_file:
     st.write(df)
 
     st.write(uploaded_file.name)
+
+df[df.columns[2]] = df[df.columns[2]].astype(str)
 
 df[df.columns[2]] = df[df.columns[2]].apply(lambda x: x.replace(",","."))
 
