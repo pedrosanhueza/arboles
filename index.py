2c426606c319d23ad695d77adf9523fd263e22ee
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-27 12:35:52 -0600

    testing1

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
-print('initial file 2')
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import matplotlib.pyplot as plt
+import altair as alt
+
+st.markdown('''
+<ol>
+<li>What is the distribution of net worth among billionaires?</li>
+<li>What is the distribution of age among billionaires?</li>
+<li>Is there a correlation between age and net worth?</li>
+<li>What are the most common industries among billionaires?</li>
+</ol>
+''',unsafe_allow_html=True)
