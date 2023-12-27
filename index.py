20aae53931813b036769934545d2a741b02a72cc
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-27 14:26:35 -0600

    file name

--- index.py
+++ index.py
@@ -1,22 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-uploaded_file = st.file_uploader("Choose a CSV file")
+uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 
 if uploaded_file:
 
-    fileName = uploaded_file.name
-    
-    st.write(fileName)
-    
-    if fileName.endswith("csv"):
-        
-        df = pd.read_csv(uploaded_file)
-
-    elif fileName.endswith("xlsx"):
-
-        df = pd.read_excel(uploaded_file, engine='openpyxl')
+    df = pd.read_csv(uploaded_file)
 
     st.write("### Uploaded CSV File:")
-    
+
     st.write(df)
+
+    st.write(uploaded_file.name)
