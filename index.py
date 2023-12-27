a6867a383338373d0ecf1ea6ba5543d1e16ab151
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-27 14:18:28 -0600

    colorado like a ring

--- index.py
+++ index.py
@@ -2,9 +2,14 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+uploaded_file = st.file_uploader("Choose a CSV file")
 
-df = pd.read_csv(uploaded_file)
+if uploaded_file:
 
-st.write("### Uploaded CSV File:")
-st.write(df)
+    st.write(uploaded_file.name)
+    
+    df = pd.read_csv(uploaded_file)
+
+    st.write("### Uploaded CSV File:")
+    
+    st.write(df)
