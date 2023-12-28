322606c3426fb5015dd381f07d37fe2190d63d7e
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 14:55:02 -0600

    namefile

--- index.py
+++ index.py
@@ -4,6 +4,9 @@ import pandas as pd
 uploaded_file = st.file_uploader("Upload a file", type=['csv', 'xlsx'])
 
 if uploaded_file:
+
+    
+    st.write(uploaded_file.name)
     
     df = pd.read_excel(uploaded_file)
 
