ff9cc0e19829c9c64f7e26eb55a75d482a5f395f
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 13:17:21 -0600

    cc

--- index.py
+++ index.py
@@ -1,31 +1,3 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
-# bootstrap 4 collapse example
-components.html(
-    """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Excel File Upload Example</title>
-</head>
-<body>
-
-    <h1>Excel File Upload Form</h1>
-
-    <form action="/upload" method="post" enctype="multipart/form-data">
-        <label for="excelFile">Choose an Excel file:</label>
-        <input type="file" id="excelFile" name="excelFile" accept=".xls, .xlsx">
-
-        <br>
-
-        <input type="submit" value="Upload">
-    </form>
-
-</body>
-</html>
-    """,
-    height=600,
-)
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
