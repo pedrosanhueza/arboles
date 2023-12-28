5df21a5c2205debcb165174f37419d3fe7aea671
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 12:44:50 -0600

    bb

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from io import StringIO, BytesIO
+from io import BytesIO
 
 st.title("Excel File Reader")
 uploaded_file = st.file_uploader("Choose a file")
@@ -9,23 +9,19 @@ if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
 
-    # To convert to a string-based or binary IO:
-    if uploaded_file.encoding is not None:
-        # Text file, decode as UTF-8
-        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-        st.write(stringio)
+    # To convert to a binary IO:
+    bytesio = BytesIO(uploaded_file.getvalue())
+    st.write(bytesio)
 
-        # To read file as a string:
-        string_data = stringio.read()
-        st.write(string_data)
-    else:
-        # Binary file, use BytesIO
-        bytesio = BytesIO(uploaded_file.getvalue())
-        st.write(bytesio)
-
-        # Handle binary file as needed, e.g., for Excel files
-        if uploaded_file.name.endswith(('.xls', '.xlsx')):
+    # Handle both text and binary files using Pandas functions
+    try:
+        # Try reading as CSV
+        dataframe = pd.read_csv(bytesio)
+        st.dataframe(dataframe)  # Display DataFrame
+    except pd.errors.ParserError:
+        try:
+            # Try reading as Excel
             dataframe = pd.read_excel(bytesio, engine='openpyxl')
             st.dataframe(dataframe)  # Display DataFrame
-        else:
-            st.warning("Unsupported binary file format. Please upload a text file or Excel file.")
+        except pd.errors.ParserError:
+            st.warning("Unsupported file format. Please upload a CSV or Excel file.")
