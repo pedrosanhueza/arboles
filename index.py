ea5a1fe81e20c064fbbd2b9dde72487f31e0f4db
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 13:35:18 -0600

    zeros

--- index.py
+++ index.py
@@ -1,15 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-uploadedFile = st.file_uploader("fileUploadLabel", type=['csv','xlsx'],accept_multiple_files=False,key="fileUploader")
+uploadedFile = st.file_uploader("fileUploadLabel", type=['csv','xlsx'])
 
-try:
-    df=pd.read_csv(uploadedFile, error_bad_lines=True, warn_bad_lines=False)
-except:
-    try:
-            df = pd.read_excel(uploadedFile)
-    except:      
-            df=pd.DataFrame()
-
-if df:
-    st.table(df)
+st.write(uploadedFile.name)
