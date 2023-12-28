4b0aba194df660fe3a1ff59e9cea057e8b197f6f
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 14:48:05 -0600

    commit

--- index.py
+++ index.py
@@ -4,4 +4,9 @@ import streamlit as st
 uploaded_file = st.file_uploader("Upload a file", type=['csv', 'xlsx'])
 
 if uploaded_file:
+    
+    st.write(uploaded_file.name)
+
     df = pd.read_excel(uploaded_file)
+
+    st.table(df)
