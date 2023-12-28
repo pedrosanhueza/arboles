781c2addff80fd923ab2a3556a6f18decac0cd29
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 12:31:28 -0600

    waiting

--- index.py
+++ index.py
@@ -66,16 +66,25 @@ def read_excel(file):
         st.error(f"Error reading Excel file: {e}")
     return df
 
-
 st.title("Excel File Reader")
 
-uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls"])
+from io import StringIO
+
+uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
-    df = read_excel(uploaded_file)
-    if not df.empty:
-        st.dataframe(df)
-    else:
-        st.warning("Selected sheet is empty or could not be loaded.")
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
 
-st.table(df)
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
