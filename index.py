e91f229be067fe43f40d98c20f5fc8fb76b54840
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 15:49:11 -0600

    commit

--- index.py
+++ index.py
@@ -3,26 +3,6 @@ import pandas as pd
 
 st.title("I-tree Candelaria")
 
-uploaded_file = st.file_uploader("",type=['csv', 'xlsx'])
-
-if uploaded_file:
-
-    fileName = uploaded_file.name    
-    
-    if fileName.endswith("xlsx"):
-
-        df = pd.read_excel(uploaded_file)
-
-    elif fileName.endswith("csv"):
-    
-        df = pd.read_csv(uploaded_file)
-
-    else:
-
-        st.warning('Error in file upload', icon="⚠️")
-    
-    st.table(df)
-
 with st.sidebar:
     
     lai = st.number_input('Leaf area index (LAI)', value=0.15)
@@ -33,7 +13,24 @@ col1, col2 = st.columns(2)
 col1.metric("Leaf area index", lai)
 col2.metric("Superficie Cubierta" , f"{tc}%")
 
-if df:
-    
-    st.write("Raw dataframe")
-    st.dataframe(df)
+uploaded_file = st.file_uploader("",type=['csv', 'xlsx'])
+
+# if uploaded_file:
+
+fileName = uploaded_file.name    
+
+if fileName.endswith("xlsx"):
+
+    df = pd.read_excel(uploaded_file)
+
+elif fileName.endswith("csv"):
+
+    df = pd.read_csv(uploaded_file)
+
+else:
+
+    st.warning('Error in file upload', icon="⚠️")
+
+if uploaded_file:
+
+    st.table(df)
