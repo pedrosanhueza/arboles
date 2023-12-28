a36af95515f5c41461502f54f845641e1831eee7
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 13:58:54 -0600

    dev

--- index.py
+++ index.py
@@ -3,14 +3,3 @@ import streamlit as st
 
 uploaded_file = st.file_uploader("Upload a file", type=['csv', 'xlsx'])
 
-if uploaded_file:
-    # Load the Excel workbook using load_workbook
-    wb = load_workbook(uploaded_file)
-
-    # Choose the sheet you want to read
-    sheet_name = "Sheet1"
-
-    # Read the selected sheet into a DataFrame
-    df = pd.read_excel(wb, sheet_name=sheet_name)
-
-    # Now you can work with the DataFrame 'df'
