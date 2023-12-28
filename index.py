b81129519c2153d32248655dbd131a24b7840028
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 13:48:22 -0600

    commit

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
-import streamlit as st
+from openpyxl import load_workbook
 import pandas as pd
-from io import StringIO
+import streamlit as st
 
 uploaded_file = st.file_uploader("Upload a file", type=['csv', 'xlsx'])
 
 if uploaded_file:
-
     df = pd.read_excel(uploaded_file, sheet_name="Sheet1")
+    # Now you can work with the DataFrame 'df'
