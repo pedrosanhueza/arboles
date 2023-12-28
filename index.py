754da49961ea505efd33522dd4c0667eb62f0599
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 14:56:37 -0600

    commit2

--- index.py
+++ index.py
@@ -5,8 +5,15 @@ uploaded_file = st.file_uploader("Upload a file", type=['csv', 'xlsx'])
 
 if uploaded_file:
 
+    fileName = uploaded_file.name    
     
-    st.write(uploaded_file.name)
+    if fileName.endswith("xlsx"):
+
+        st.write("xlsx")
+
+    elif fileName.endswith("csv"):
+
+        st.write("csv")
     
     df = pd.read_excel(uploaded_file)
 
