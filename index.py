ee4203ec0b07bf46f15ca41dc40eb2acfa2320c9
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 12:59:22 -0600

    cc

--- index.py
+++ index.py
@@ -1,25 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-def read_excel(file_path):
-    try:
-        df = pd.read_excel(file_path, engine='openpyxl')
-        return df
-    except Exception as e:
-        st.error(f"Error: {e}")
-        return None
-
-def main():
-    st.title("Excel Reader App")
-
-    uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx"])
-
-    if uploaded_file is not None:
-        st.subheader("Preview of Data:")
-        df = read_excel(uploaded_file)
-
-        if df is not None:
-            st.dataframe(df)
-
-if __name__ == "__main__":
-    main()
+st.markdown('''
+<h2>Hola</h2>
+''', unsafe_allow_html=False)
