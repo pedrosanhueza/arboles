7e06db783da1bca58bbe55b1f8ebc8c5de469668
Pedro Sanhueza <pedroasd1205@gmail.com>
2023-12-28 13:20:15 -0600

    cc

--- index.py
+++ index.py
@@ -3,4 +3,6 @@ import pandas as pd
 
 uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
 
-df = pd.read_excel(uploaded_files )
+if uploaded_files:
+    
+    df = pd.read_excel(uploaded_files )
